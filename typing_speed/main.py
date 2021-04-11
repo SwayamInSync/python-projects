f691dc3a42cf3ca632cdf7a56d334c8770d505e2
Swayam <74960567+practice404@users.noreply.github.com>
2021-04-11 20:33:16 +0530

    update main.py
    functionality of telling number of mistakes is added

--- typing_speed/main.py
+++ typing_speed/main.py
@@ -4,7 +4,7 @@ from tkinter import *
 root = Tk()
 root.title("typing speed calculator")
 paragraph = "Lets keep typing and see how much is your speed, its a very minimal program to judge your typing in words per minute"
-
+entry = Entry(width=100)
 
 def start(target):
     global x
@@ -19,13 +19,19 @@ def done():
     time_taken = y - x
     words_per_min = (number_words / time_taken) * 60
     ans = round(words_per_min, 2)
-    Label(root, text=ans).grid(row=4, column=0)
+    # calculating number of mistakes
+    user_text = entry.get()
+    user_words = user_text.split(" ")
+    wrong_words = 0
+    for word in user_words:
+        if word not in words:
+            wrong_words += 1
+    Label(root, text=f"WPM: {ans}\nMistakes: {wrong_words}").grid(row=4, column=0)
 
 
 def start_typing():
     start_btn.destroy()
     label = Label(root, text=paragraph).grid(row=0, column=0)
-    entry = Entry(width=100)
     entry.grid(row=1, column=0)
     entry.bind("<KeyPress>", lambda event: start(target=entry))
     done_btn = Button(root, text="complete", command=done)
