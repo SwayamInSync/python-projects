b5d5fa97581e98f013f92ffd6a7bca980e13ffe1
Swayam <74960567+practice404@users.noreply.github.com>
2021-04-28 17:37:11 +0530

    Update main.py
    feature added:
    can handle multiple birthdays on same day

--- birthday-wisher/main.py
+++ birthday-wisher/main.py
@@ -1,35 +1,40 @@
-##################### Extra Hard Starting Project ######################
 import datetime as dt
 import pandas
 import random
 import smtplib
 
-my_email = "email@gmail.com"
+my_email = "your_email@gmail.com"
 passw = "your_password"
-# 1. Update the birthdays.csv
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
 
 data = pandas.read_csv("birthdays.csv")
 today = dt.datetime.now()
-bday = data[(data.month == today.month) & (data.day == today.day)]# in pandas use bitwise operator &(and) |(or) for logical operation
-name = bday["name"].to_string(index=False)
-email = bday["email"].to_string(index=False)
+bday = data[(data.month == today.month) & (data.day == today.day)]
+name = bday["name"].tolist()
+email = bday["email"].tolist()
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+friends = []
 
-num = random.randint(1,3)
-with open(f"letter_templates/letter_{num}.txt") as letter:
-    lines = letter.readlines()
-    lines[0].strip()
-    lines[0] = lines[0].replace("[NAME]", name)
-    message = "".join(lines)
+for n in range(len(name)):
+    friends.append(
+        {
+            "name": name[n],
+            "email": email[n]
+        }
+    )
 
+if not friends:
+    print("no birthday")
+else:
+    for friend in friends:
+        num = random.randint(1, 3)
+        with open(f"letters/letter_{num}.txt") as letter:
+            lines = letter.readlines()
+            lines[0].strip()
+            lines[0] = lines[0].replace("[NAME]", friend["name"])
+            message = "".join(lines)
 
-# 4. Send the letter generated in step 3 to that person's email address.
-
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=passw)
-    connection.sendmail(from_addr=my_email, to_addrs=email, msg=f"Subject: HAPPY BIRTHDAY\n\n{message}")
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=passw)
+            connection.sendmail(from_addr=my_email, to_addrs=friend["email"], msg=f"Subject: HAPPY BIRTHDAY\n\n{message}")
+            print(f"message sent to {friend['name']}")
