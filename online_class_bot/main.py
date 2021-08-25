4515c8a185d86fbbd4d42b64283a33d6223d5550
Swayam <74960567+practice404@users.noreply.github.com>
2021-08-25 23:29:51 +0530

    bot V3 added

---
+++ online_class_bot/main.py
@@ -0,0 +1,76 @@
+import time
+import os
+import pyautogui
+import webbrowser
+import pandas as pd
+from datetime import datetime, timedelta
+
+pyautogui.FAILSAFE = True
+
+"""
+Before using it remember it only work for google meet, also you need to change the path to your chrome browser.
+replace the (x,y) coordinated inside pyautogui.click() to your screen's respective coordinates.
+"""
+
+
+def open_chrome(meet_code, duration):
+    chrome_path = 'open -a /Applications/Google\ Chrome.app %s'  # replcae the path to your chrome browser
+    if webbrowser.get(chrome_path).open("https://meet.google.com/"):
+        pyautogui.sleep(3)
+        pyautogui.click(323, 570)  # coordinate for meet_code input
+        pyautogui.write(meet_code)
+        pyautogui.sleep(1.2)
+        pyautogui.press('enter')
+        pyautogui.sleep(5)
+        pyautogui.hotkey('command', 'e')  # stop the video
+        pyautogui.hotkey('command', 'd')  # mute yourself
+        pyautogui.sleep(5)
+        pyautogui.click(1029, 488)  # coordinate for joining class button
+
+        joining_time = datetime.now()
+        ending_time = joining_time + timedelta(seconds=duration)
+        while True:
+            if datetime.now().time().hour == ending_time.hour and datetime.now().minute == ending_time.minute and datetime.now().second == ending_time.second:
+                os.system("killall -9 'Google Chrome'")
+                break
+            else:
+                time.sleep(1)
+
+
+def find_class():
+    duration_in_seconds = 0
+    df = pd.read_csv('class_links.csv')
+    today = datetime.now()
+    time_ = today
+    diff_time = timedelta(minutes=5)
+    upper_time_range = time_ + diff_time
+    lower_time_range = time_ - diff_time
+
+    meet_code = ""
+    for row in df.itertuples():
+        Index, hour, minute, code, duration_in_seconds = row
+        new_time = datetime(hour=hour, minute=minute, year=datetime.now().year, month=datetime.now().month,
+                            day=datetime.now().day)
+        if lower_time_range.time() <= new_time.time() <= upper_time_range.time():
+            meet_code = code
+
+    if meet_code:
+        open_chrome(meet_code, duration_in_seconds)
+        return duration_in_seconds
+    return False
+
+
+"""
+for windows crontab don't work so use while loop below
+"""
+
+while True:
+    print("searching class...")
+    sec = find_class()
+    if sec:
+        print("class found, joining")
+        time.sleep(10)
+    else:
+        print("class not found")
+        print("sleeping for 1 hour")
+        time.sleep(60 * 60)
